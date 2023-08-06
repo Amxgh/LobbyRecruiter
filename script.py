38de3645dad454ba7dd7527f48c1533626c003bf
Amxgh <86120332+Amxgh@users.noreply.github.com>
2023-08-06 22:03:23 +0400

    Feat: Made some changes and made it specifically for Miscellaneous' requirements

--- script.py
+++ script.py
@@ -1,116 +1,112 @@
 import os
+import random
 import re
-import requests
 import threading
 
+import requests
 
-# This allows you to access the hypixel api.
-# To get a key, go to https://developer.hypixel.net/ and follow the instructions.
-API_KEY = ''
+# To get an api key, go to https://developer.hypixel.net/ and follow the instructions.
+# For badlion the Chat logs can be found under `.minecraft/logs/blclient/minecraft/latest.log` (untested)
 
-# Here we attempt to work out where your chat logs file is.
-# If this doesn't work, you will need to replace the path variable
-# with the correct value. Make sure to keep the 'fr' before the string.
+# CONSTANTS
+API_KEYS = []
+USER_PROFILE = os.getenv('USERPROFILE')
+PATH = fr'{USER_PROFILE}\.lunarclient\offline\multiver\logs\latest.log'
 
-#For badlion the Chat logs can be found under `.minecraft/logs/blclient/minecraft/latest.log` (untested)
-user_profile = os.getenv('USERPROFILE')
-path = fr'{user_profile}\.lunarclient\offline\multiver\logs\latest.log' 
+REQUIRED_GEXP = 100000
+MAX_GUILD_LEVEL_TO_INVITE = 150
+
+# ! Below are constant variables, do not touch.
+# The below is used to calculate the guild's level.
+LEVEL_UP_EXP_NEEDED = [
+    100000,
+    150000,
+    250000,
+    500000,
+    750000,
+    1000000,
+    1250000,
+    1500000,
+    2000000,
+    2500000,
+    2500000,
+    2500000,
+    2500000,
+    2500000,
+    3000000]
+
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def get_hyapi_key():
+    return random.choice(API_KEYS)
+
+
+def get_weekly_gexp(uuid, guild_data):
+    for member in guild_data["members"]:
+        if member["uuid"] == uuid:
+            return sum(member["expHistory"].values())
+    return None
+
+
+def get_guild_level_from_exp(exp):
+    level = 0
+
+    for req in LEVEL_UP_EXP_NEEDED:
+
+        if exp >= req:
+            exp -= req
+            level += 1
+
+    level += int(exp / LEVEL_UP_EXP_NEEDED[-1])
+
+    return level
+
 
 def check_stats(player):
     res = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{player}?').json()
     try:
         uuid = res['id']
     except KeyError:
-        print(f'{player} does not meet requirements.')
         return False
-        
-    #Getting data from hypixel
+
+    API_KEY = get_hyapi_key()
+
+    # Getting data from hypixel
     headers = {"API-Key": API_KEY}
     guild_data = requests.get(
         f'https://api.hypixel.net/guild?player={uuid}',
         headers=headers).json()
+    if guild_data and "guild" in guild_data:
+        if guild_data["guild"]:
+            guild_data = guild_data['guild']
+            guild_level = get_guild_level_from_exp(guild_data['exp'])
+            weekly_gexp = get_weekly_gexp(uuid, guild_data)
 
-    data = requests.get(
-        f"https://api.hypixel.net/player?uuid={uuid}",
-        headers=headers).json()
-    
-    #Getting the players general stats
-    try:
-        stats_data: dict = data.get('player').get('stats', {})
-    except (KeyError, ValueError):
-        print(f'{player} does not meet requirements.')
-        return False
-
-    bedwars_data = stats_data.get('Bedwars', {})
-    duels_data = stats_data.get('Duels', {})
-
-    #Working out their bedwars stats
-    try:
-        bedwars_star = data["player"]["achievements"]["bedwars_level"]
-    except KeyError:
-        bedwars_star = 0
-    
-    if bedwars_star < 50:
-        print(f'{player} does not meet requirements.')
-        return False
-
-    bw_final_kills = bedwars_data.get('final_kills_bedwars', 0)
-    bw_final_deaths = bedwars_data.get('final_deaths_bedwars', 0)
-    bw_fkdr = bw_final_kills / (bw_final_deaths or 1)
-
-    guild_data = guild_data['guild']
-
-    #Calculating index
-    index = bedwars_star * (bw_fkdr**2)
-
-    if guild_data != None:
-        
-        exp = guild_data['exp']
-        
-        EXP_NEEDED = [
-        100000,
-        150000,
-        250000,
-        500000,
-        750000,
-        1000000,
-        1250000,
-        1500000,
-        2000000,
-        2500000,
-        2500000,
-        2500000,
-        2500000,
-        2500000,
-        3000000]
-        
-        level = 0
-        
-        for req in EXP_NEEDED:
-            
-            if exp >= req:
-                exp -= req
-                level += 1
-                
-        level += int(exp/EXP_NEEDED[-1])
-        
-        if level > 30:
-            print(f'{player} does not meet requirements.')
+            if (guild_level < MAX_GUILD_LEVEL_TO_INVITE) and (weekly_gexp > REQUIRED_GEXP):
+                print(f"Message and recruit: {colors.OKBLUE} {player} {colors.ENDC} ({weekly_gexp})\n")
+                return True
             return False
 
-    #Checking if their index meets requirements
-    if index >= 2000:
-        print(f'{player} meets requirements!    TRUE')
+        print(f"Send an invite to: {colors.OKGREEN} {player} {colors.ENDC}\n")
         return True
-    print(f'{player} does not meet requirements.')
-    return False
 
 
 def find_players():
-    print('Starting..\n')
+    print(colors.BOLD + 'Starting..\n')
 
-    #Opening the logs file
-    with open(path, 'r+b') as file:
+    # Opening the logs file
+    with open(PATH, 'r+b') as file:
         for line_bytes in file.readlines():
             line = str(line_bytes).split('[CHAT] ')[-1]  # Only message content
             line = re.sub(r'\\[nrt]', '', line)  # Remove all escape characters
